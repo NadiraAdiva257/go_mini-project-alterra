87f681424cce7be2b3f32e54e301b0625d967d09
NadiraAdiva257 <adivanadira257@gmail.com>
2023-05-07 10:14:09 +0700

    edit config untuk docker

--- mini-project/config/config.go
+++ mini-project/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"mini-project/model"
 
 	"gorm.io/driver/mysql"
@@ -25,25 +26,25 @@ type Config struct {
 }
 
 func InitDB() {
-	// config := Config{
-	// 	DB_Username: "root",
-	// 	DB_Password: "",
-	// 	DB_Port:     "3306",
-	// 	DB_Host:     "localhost",
-	// 	DB_Name:     "mini-project",
-	// }
+	config := Config{
+		DB_Username: "admin",
+		DB_Password: "miniproject",
+		DB_Port:     "3306",
+		DB_Host:     "database-mini-project.cfhwnsmwbh2n.ap-southeast-2.rds.amazonaws.com",
+		DB_Name:     "mini_project",
+	}
 
-	// connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-	// 	config.DB_Username,
-	// 	config.DB_Password,
-	// 	config.DB_Host,
-	// 	config.DB_Port,
-	// 	config.DB_Name,
-	// )
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		config.DB_Username,
+		config.DB_Password,
+		config.DB_Host,
+		config.DB_Port,
+		config.DB_Name,
+	)
 
 	var err error
-	// DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
-	DB, err = gorm.Open(mysql.Open("root:@tcp(host.docker.internal)/mini-project?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	// DB, err = gorm.Open(mysql.Open("root:@tcp(host.docker.internal)/mini-project?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
