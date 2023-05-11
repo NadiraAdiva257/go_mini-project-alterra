a037502746ed1a40686cad563989807b6bedcca0
NadiraAdiva257 <adivanadira257@gmail.com>
2023-05-11 10:24:43 +0700

    coba add unit testing untuk get debt by creditor

--- service/debt-service-mock.go
+++ service/debt-service-mock.go
@@ -3,8 +3,10 @@ package service
 import (
 	"errors"
 	"mini-project/model"
+	"time"
 
 	"github.com/stretchr/testify/mock"
+	"gorm.io/datatypes"
 )
 
 type DebtRepositoryMock struct {
@@ -26,5 +28,35 @@ func (dm *DebtRepositoryMock) DeleteDebtController(id, debtor_id int) error {
 		return result
 	} else {
 		return errors.New("error")
+	}
+}
+
+func (dm *DebtRepositoryMock) GetDebtByCreditorController(creditor_name string, debtor_id int) (map[string]interface{}, error) {
+	var debt []model.Debt
+	var result = make(map[string]interface{})
+
+	for _, val := range debt {
+		if val.CreditorName == creditor_name && val.DebtorID == debtor_id {
+			result["total debt"] = 23000
+			result["mayla"] = []model.Debt{
+				{
+					Date:   datatypes.Date(time.Now()),
+					Amount: 23000,
+					Detail: "cilok",
+				},
+				{
+					Date:   datatypes.Date(time.Now()),
+					Amount: 23000,
+					Detail: "cilok",
+				},
+			}
+			break
+		}
+	}
+
+	if len(result) != 0 {
+		return result, nil
+	} else {
+		return nil, errors.New("data empty")
 	}
 }
--- service/debt-service.go
+++ service/debt-service.go
@@ -1,12 +1,16 @@
 package service
 
 import (
+	"log"
 	"mini-project/config"
 	"mini-project/model"
+
+	"gorm.io/datatypes"
 )
 
 type IDebtService interface {
 	DeleteDebtController(id, debtor_id int) error
+	GetDebtByCreditorController(creditor_name string, debtor_id int) (map[string]interface{}, error)
 }
 
 type DebtRepository struct {
@@ -16,7 +20,7 @@ type DebtRepository struct {
 var debtRepository IDebtService
 
 func init() {
-	dr := &DebtorRepository{}
+	dr := &DebtRepository{}
 	dr.Func = dr
 
 	debtRepository = dr
@@ -30,7 +34,7 @@ func SetDebtRepository(dr IDebtService) {
 	debtRepository = dr
 }
 
-func (d *DebtorRepository) DeleteDebtController(id, debtor_id int) error {
+func (d *DebtRepository) DeleteDebtController(id, debtor_id int) error {
 	var debt model.Debt
 
 	if err := config.DB.Where("id = ? AND debtor_id = ?", id, debtor_id).Delete(&debt); err != nil {
@@ -38,4 +42,41 @@ func (d *DebtorRepository) DeleteDebtController(id, debtor_id int) error {
 	}
 
 	return nil
+}
+
+type Result1 struct {
+	CreditorName string
+	Amount       int
+	Detail       string
+}
+
+type Result2 struct {
+	Date   datatypes.Date
+	Amount int
+	Detail string
+}
+
+type ResultTotal struct {
+	Total int
+}
+
+func (d *DebtRepository) GetDebtByCreditorController(creditor_name string, debtor_id int) (map[string]interface{}, error) {
+	var debt model.Debt
+	var resultTotal []ResultTotal
+	var result2 []Result2
+
+	if err := config.DB.Model(&debt).Select("sum(amount) AS total").Where("creditor_name = ? AND debtor_id = ?", creditor_name, debtor_id).Find(&resultTotal); err != nil {
+		return nil, err.Error
+	}
+
+	if err := config.DB.Model(&debt).Where("creditor_name = ? AND debtor_id = ?", creditor_name, debtor_id).Find(&result2); err != nil {
+		return nil, err.Error
+	}
+
+	log.Println(result2)
+
+	return map[string]interface{}{
+		"total debt":  resultTotal,
+		creditor_name: result2,
+	}, nil
 }
